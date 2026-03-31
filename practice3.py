51be2f5cad5af4e332b63a4d9a3ec922456ea397
ISols2336 <yyt2336@outlook.com>
2026-03-31 23:00:45 +0800

    运算符练习

---
+++ practice3.py
@@ -0,0 +1,24 @@
+#赋值运算符
+a = 15
+b = 5
+a += b        # 相当于：a = a + b
+a *= a + 2    # 相当于：a = a * (a + 2)
+print(a)  
+
+#海象运算符
+print((c:=114)) #将右侧的值赋给左边变量
+print(c)
+
+#比较运算符和逻辑运算符and,or,not
+
+#比较运算符
+flag0=1==1
+flag1=6>2
+flag2=4<1
+flag3=1!=3
+print('flag0=',flag0)
+print('flag1=',flag1)
+print('flag2=',flag2)
+print('flag3=',flag3)
+
+#逻辑运算符
