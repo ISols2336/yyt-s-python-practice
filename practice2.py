b2155c18e226c057cc1f4f51e675e390564f0a14
ISols2336 <yyt2336@outlook.com>
2026-03-31 22:17:57 +0800

    第二次练习

---
+++ practice2.py
@@ -0,0 +1,17 @@
+a=100                  #为a赋值100
+b=114.514              #为b赋值114.514（浮点数）
+c='我的python练习'      #为c赋值字符串类型（str）  
+d=''                   #为d赋值，无内容字符串
+e='d'                    #为e赋值为d  
+
+print(float(a))        #转换为浮点数打印a
+
+print(int(b))          #将b转换为int整数打印
+
+print(bool(c))         #bool类型输出str，只要字符串有内容，输出内容就为True
+
+print(bool(d))         #输出为False
+
+print(chr(a))          #int类型转str类型，100为d
+
+print(ord(e))          #str类型转为int类型，d为100
