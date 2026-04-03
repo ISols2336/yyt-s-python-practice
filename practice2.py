87c3ac48e37503f7a9f26de3e12a5847d63c7fc8
ISols2336 <yyt2336@outlook.com>
2026-04-03 23:33:52 +0800

    加入str函数说明

--- practice2.py
+++ practice2.py
@@ -3,6 +3,7 @@ b=114.514              #为b赋值114.514（浮点数）
 c='我的python练习'      #为c赋值字符串类型（str）  
 d=''                   #为d赋值，无内容字符串
 e='d'                    #为e赋值为d  
+f=12
 
 print(float(a))        #转换为浮点数打印a
 
@@ -15,3 +16,5 @@ print(bool(d))         #输出为False
 print(chr(a))          #int类型转str类型，100为d
 
 print(ord(e))          #str类型转为int类型，d为100
+
+print(str(f),'天')     #转化为str字符串类型
