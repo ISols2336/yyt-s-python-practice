0b6ed7f370fb6071b5e56217e7d73383f01b774f
ISols <101307300+ISols2336@users.noreply.github.com>
2026-04-04 18:49:13 +0800

    if,elif,else运用

--- practice6.py
+++ practice6.py
@@ -1,16 +1,19 @@
 BMI=(kg:=float(input('请输入体重'))/(meter:=float(input('请输入身高'))**2))
 print(f'BMI={BMI:.2f}')
-if 18.5>BMI:
+if   BMI<18.5:
     print('你的身材偏瘦')
 
-
-
-elif 18.5<=BMI<24:
+elif BMI<24:
     print('你的身材很棒')      #关系运算会产生布尔值，如果if后面的布尔值为True，那么if语句下方，有四个空格缩进的print('你的身材很棒！')就会被执行
 
+elif BMI<27:
+    print('您有些肥胖')        #如果上方的if不成立就会elif
 
+elif BMI<30:
+    print('您轻度肥胖')
 
-
+elif BMI<35:
+    print('您重度肥胖')
 
 else:                         #if后条件不成立，就运行else
-    print('你的身材不够标准')
+    print('666良子来了')
